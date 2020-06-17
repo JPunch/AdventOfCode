5c164640becafaeab0162b11d4975ef5a34103d8
Jack Winstanley-Punch <jackwp1996@gmail.com>
2020-06-17 10:22:47 +0100

    day10 part1 complete

--- day10.py
+++ day10.py
@@ -5,19 +5,12 @@ import numpy as np
 with open("inputday10.txt", "r") as f:
     f_read = f.read().split("\n")
 
+
 class Point():
     def __init__(self,x ,y):
         self.x = x
         self.y = y
 
-# def station_location(field): # ask the Liam what he would do for assigning points to every position
-#     asteroid_field = field.copy()
-#     y = 0
-#     for i in asteroid_field:
-#         for x in range(len(i)):
-#             i[x] = Point(x, y)
-#         y += 1
-#     return asteroid_field
 
 def station_location(field): 
     asteroid_field = field.copy()
@@ -31,18 +24,26 @@ def station_location(field):
                 asteroid_points.append(Point(x, y))
         y += 1
     for i in asteroid_points:
+        location_count[f"{i.x}, {i.y}"] = 0
         for j in asteroid_points:
             if i == j:
                 continue
-            location_count[f"{i.x}, {i.y}"] = 0
-            angle = np.arctan((i.y - j.y)/(i.x - j.x))
-            if angle not in angles:
-                angles.append(angle)
-                location_count[f"{i.x}, {i.y}"] += 1
+            else:
+                angle = np.arctan2((i.y - j.y),(i.x - j.x))
+                if angle not in angles:
+                    angles.append(angle)
+                    location_count[f"{i.x}, {i.y}"] += 1
+        angles.clear()
     ans = max(location_count.values())
-    return ans
+    for a in location_count:
+        if location_count[a] == ans:
+            location = a
+    return ans, location
+
+def asteroid_bet(field):
+    total, location = station_location(field)
+    
+
 
 if __name__ == "__main__":
     print(station_location(f_read))
-
-
--- day10test.py
+++ day10test.py
@@ -2,20 +2,27 @@
 
 import unittest
 from day10 import station_location
+from day10 import asteroid_bet
 
 example1 = "......#.#./#..#.#..../..#######./.#.#.###../.#..#...../..#....#.#/#..#....#./.##.#..###/##...#..#./.#....####"
 example2 = "#.#...#.#./.###....#./.#....#.../##.#.#.#.#/....#.#.#./.##..###.#/..#...##../..##....##/......#.../.####.###."
 example3 = ".#..#..###/####.###.#/....###.#./..###.##.#/##.##.#.#./....###..#/..#.#..#.#/#..#.#.###/.##...##.#/.....#.#.."
-
+example4 = ".#..##.###...#######/##.############..##./.#.######.########.#/.###.#######.####.#./#####.##.#.##.###.##/..#####..#.#########/####################/#.####....###.#.#.##/##.#################/#####.##.###..####../..######..##.#######/####.##.####...##..#/.#####..#.######.###/##...#.##########.../#.##########.#######/.####.#.###.###.#.##/....##.##.###..#####/.#.#.###########.###/#.#.#.#####.####.###/###.##.####.##.#..##"
 class StationLocationTests(unittest.TestCase):
     def test_ex1(self):
-        return self.assertEqual(station_location(example1.split("/")), 33)
+        return self.assertEqual(station_location(example1.split("/"))[0], 33)
 
     def test_ex2(self):
-        return self.assertEqual(station_location(example2.split("/")), 35)
+        return self.assertEqual(station_location(example2.split("/"))[0], 35)
 
     def test_ex3(self):
-        return self.assertEqual(station_location(example3.split("/")), 41)
+        return self.assertEqual(station_location(example3.split("/"))[0], 41)
+
+
+class AsteroidBetTests(unittest.TestCase):
+    def test_ex1(self):
+        return self.assertEqual(asteroid_bet(example4), 802)
+
 
 if __name__ == "__main__":
     unittest.main()
