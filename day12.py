71df29ac8ddf9efa15c10d5cb971193914befb59
Jack Winstanley-Punch <jackwp1996@gmail.com>
2020-06-22 16:26:49 +0100

    day12

--- day12.py
+++ day12.py
@@ -8,15 +8,14 @@ def moon_parse(moons):
     moons = moons.split("\n")
     for pos in moons:
         pos = pos.split(",")
-        print(pos)
         moon_info.append(Moon(pos[0], pos[1], pos[2]))
     return moon_info
 
-class Vector2:
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
+# class Vector2:
+#     def __init__(self, x, y, z):
+#         self.x = x
+#         self.y = y
+#         self.z = z
 
 class Moon():
     def __init__(self, posx, posy, posz, vecx=0, vecy=0, vecz=0):
@@ -85,9 +84,35 @@ def moon_energy(moons, step_num):
     ans = [(abs(i.posx) + abs(i.posy) + abs(i.posz)) * (abs(i.vecx) + abs(i.vecy) + abs(i.vecz)) for i in moons]
     return sum(ans)
 
+def match_state(moons):
+    initial_state = [moons]
+    final_state = []
+    interaction_ls = []
+    steps = 0
+    run = True
+    while run:
+        for moon1 in moons:
+            for moon2 in moons:
+                if moon1 == moon2:
+                    continue
+                elif [moon1, moon2] in interaction_ls or [moon2, moon1] in interaction_ls:
+                    continue
+                else:
+                    moon1.gravity(moon2)
+                    interaction_ls.append([moon1, moon2])
+        for moon in moons:
+            moon.apply_velocity()
+            final_state.append(moon)
+        steps += 1
+        print(steps)
+        if initial_state == final_state:
+            return steps
+        final_state.clear()
+        interaction_ls.clear()
 
 if __name__ == "__main__":
     with open("inputday12.txt", "r") as f:
         f_read = f.read()
-    # print(moon_parse(f_read))
-    print(moon_energy(moon_parse(f_read), 1000))
+    input = moon_parse(f_read)
+    print(moon_energy(input, 1000))
+    # match_state(input)
--- day12test.py
+++ day12test.py
@@ -1,7 +1,7 @@
 '''Advent of code 2019 day12 tests'''
 
 import unittest
-from day12 import moon_parse, moon_energy
+from day12 import moon_parse, moon_energy, match_state
 
 class MoonEnergy(unittest.TestCase):
     def testex1(self):
@@ -9,6 +9,13 @@ class MoonEnergy(unittest.TestCase):
         moons = moon_parse(example1)
         energy = moon_energy(moons, 100)
         return self.assertEqual(energy, 1940)
+
+class MatchState(unittest):
+    def textex1(self):
+        example1 = "<x=-1, y=0, z=2>\n<x=2, y=-10, z=-7>\n<x=4, y=-8, z=8>\n<x=3, y=5, z=-1>"
+        moons = moon_parse(example1)
+        steps = match_state(moons)
+        return self.assertEqual(steps, 2772)
 
 if __name__ == "__main__":
     unittest.main()
