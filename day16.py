7d4c203c49ec9fd5a4e721749576d7f5a319e04c
Jack Winstanley-Punch <jackwp1996@gmail.com>
2020-07-01 22:40:42 +0100

    I thought i had something... I didn't

--- day16.py
+++ day16.py
@@ -18,6 +18,14 @@ def last_digit(number): #returns last digit
     number = list(number)
     return number.pop()
 
+def sets_to_answer(message_start=5979187, len_input=650, repeats=10000):
+    into_set1 = 5979187 % 650
+    # print(into_set1)
+    into_set2 = into_set1 + 8
+    # print(into_set2)
+    return  repeats - (message_start//len_input) - 1
+#801 sets back from the firsst 650 and then 5979187 % 650 into it is the starting letter
+
 
 class Signal():
     def __init__(self, signal):
@@ -58,15 +66,45 @@ class Pattern():
 def d16p1():
     with open("inputday16.txt", "r") as f:
         sig = f.read()
-    signal = Signal(sig)
+    # print(len(sig))
+    # signal = Signal(sig)
+    signal = Signal("1231245595"*2)
     pattern = Pattern("0,1,0,-1")
+    desired_phase = 1
+    count = 0
+    while count < desired_phase:
+        new_signal = apply_pattern(signal, pattern)
+        signal = Signal(new_signal)
+        count += 1
+    return new_signal[-8:]
+
+def d16p2():
+    with open("inputday16.txt", "r") as f:
+        sig = f.read()
+    # signal = Signal(sig*2)
+    signal = Signal(sig*4)
+    heck = len(signal.signal)
+    pattern = Pattern("0,1,0,-1")
+    amount_to_add = sets_to_answer()
     desired_phase = 100
     count = 0
     while count < desired_phase:
         new_signal = apply_pattern(signal, pattern)
         signal = Signal(new_signal)
         count += 1
-    return new_signal[:8]
+        print(count)
+    print(new_signal[-1950:-1300], new_signal[-1300:-650], new_signal[-650:])
+    # mid_ans = []
+    # for num in chunk[487:495]:
+    #     mid_ans.append(last_digit(int(num) + 5))
+    # return "".join(mid_ans)
 
+
+#5979187 idea it seems that every 2 repeats sets one set of the input from back to front
+# so assuming that holds then at 10000 repeats the last 50000 sets of input are set
+#and then what else i noticed was that the difference between the first one set and the next one
+#is that you just add 1 to each individual digit but the amount changes
 if __name__ == "__main__":
-    print(d16p1())
+    # print(d16p1())
+    # print(sets_to_answer())
+    print(d16p2())
