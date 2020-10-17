87a928457e175a1e348dc65c6162746ca38cc4ab
Jack Winstanley-Punch <jackwp1996@gmail.com>
2020-10-17 17:08:02 +0100

    simplifying process needs tweaks

--- day14.py
+++ day14.py
@@ -1,49 +1,78 @@
 import re
 
 
-def fuel_parse(fuel_ls):
-    '''
-    Returns a dictionary with key of right hand side of the equation made into the element class
-    and a key value of all the entries on the left hand side made into the element class
-    '''
-    fuel_dict = {}
+class Equation():
+    def __init__(self, left, right):
+        self.product_value, self.product = self.parse_product(right)
+        self.reactants, self.reactant_dict = self.parse_reactants(left)
+
+    def parse_product(self, right):
+        match = re.findall("\d+\s\w+", right)
+        return match[0].split()
+
+    def parse_reactants(self, left):
+        reactants, reactant_dict = [], {}
+        matches = re.findall("\d+\s\w+", left)
+        for match in matches:
+            value, name = match.split()
+            reactants.append(name)
+            reactant_dict[name] = value
+        return reactants, reactant_dict
+    
+    def __str__(self):
+        return f"Product {self.product_value} {self.product} => {self.reactant_dict.keys(), self.reactant_dict.values()}"
+
+
+class Storage():
+    def __init__(self):
+        self.equations = []
+        self.simplify = []
+
+    def add_equation(self, equation):
+        self.equations.append(equation)
+
+    def add_simplify(self, simplify):
+        self.equations.append(simplify)
+
+
+def fuel_parse(fuel_ls, storage):
     lines = re.sub(",", "", fuel_ls)
     lines = lines.split("\n")
     for line in lines:
-        matches = re.findall("(\d+\s\w+)", line)
         left, right = line.split("=>")
-        reactants = re.findall("(\d+\s\w+)", left)
-        value, product = right.split()
-        fuel_dict[product] = {"value": value, "reactants": reactants}
-    return fuel_dict
+        storage.add_equation(Equation(left, right))        
 
-def calc_fuel(fuel_ls):
+    return storage
+
+
+def simplify(equation):
+    pass
+
+def foobar(storage):
     run = True
-    current_elements = []
-    next_elements = []
-    current_dict = ["FUEL"]
+    for equation in storage.equations:
+        if equation.product == "FUEL":
+            final_reaction = equation
     while run == True:
-        count = 1
-        for item in fuel_ls.keys():
-            for element in current_dict:
-                if(element in fuel_ls[item]["reactants"] and element not in "1 FUEL"):
-                    next_elements.extend(fuel_ls[item]["reactants"])
-        print(next_elements)
-        print("\n")
-        current_dict = next_elements
-        next_elements.clear()
-        count ++ 1
-        if(len(next_elements) == 0):
-            run = False    
-            
+
+        for equation in storage.equations:
+            if equation.product in final_reaction.reactants:
+                final_reaction.reactants.remove(equation.product)
+                final_reaction.reactants.extend(equation.reactants)
+                # final_reaction.reactant_dict[equation.product] = equation.reactants
         
+    return None #NOTHING
+
+# ANSWER FOR THE EXAMPLE IS 165 ORE
+
 
 #nect step is to regex match each number + reactant name combo to be used when creating a list to search for the next set of items
 if __name__ == "__main__":
-    with open("inputday14.txt", "r") as f:
+    with open("inputday14ex.txt", "r") as f:
         lines = f.read()
     # print(lines)
-    
-    fuel_ls = fuel_parse(lines)
+    storage = Storage()
+    fuel_ls = fuel_parse(lines, storage)
+    foobar(fuel_ls)
     # ore_count = calc_fuel(fuel_ls)
-    calc_fuel(fuel_ls)
+    # calc_fuel(fuel_ls)
