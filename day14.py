6b73a7cde50a6a897012379c57ba8494b2a41a54
Jack Winstanley-Punch <jackwp1996@gmail.com>
2020-10-17 20:04:36 +0100

    commit

--- day14.py
+++ day14.py
@@ -1,14 +1,5 @@
 import re
 
-<<<<<<< HEAD
-#
-def fuel_parse(fuel_ls):
-    '''
-    Returns a dictionary with key of right hand side of the equation made into the element class
-    and a key value of all the entries on the left hand side made into the element class
-    '''
-    fuel_dict = {}
-=======
 
 class Equation():
     def __init__(self, left, right):
@@ -45,7 +36,6 @@ class Storage():
 
 
 def fuel_parse(fuel_ls, storage):
->>>>>>> 87a928457e175a1e348dc65c6162746ca38cc4ab
     lines = re.sub(",", "", fuel_ls)
     lines = lines.split("\n")
     for line in lines:
@@ -60,39 +50,6 @@ def simplify(equation):
 
 def foobar(storage):
     run = True
-<<<<<<< HEAD
-    current_elements = []
-    next_elements = []
-    current_dict = ["1 FUEL"]
-    while run == True:
-        count = 1
-        for element in current_dict:
-            value, name = element.split()
-            if name in fuel_ls.keys():
-                next_elements.extend(fuel_ls[name]["reactants"])
-        print(next_elements)
-        print("\n")
-        current_dict = next_elements[:]
-        count ++ 1 
-        if(len(next_elements) == 0):
-            run = False   
-        next_elements.clear()
-
-            
-
-class Reaction():
-    def __init__(self, product, product_value, reactants):
-        self.product = product
-        self.product_value = product_value
-        self.reactants = []
-        self.reactant_values = []
-
-    def parse_reactants(self, reactants):
-        matchs = re.findall("(\d+\s\w+)", reactants)
-        for match in matchs:
-            self.reactants.extend(match.split()[0])
-            self.reactant_values.extend(match.split()[1])
-=======
     for equation in storage.equations:
         if equation.product == "FUEL":
             final_reaction = equation
@@ -108,7 +65,6 @@ class Reaction():
 
 # ANSWER FOR THE EXAMPLE IS 165 ORE
 
->>>>>>> 87a928457e175a1e348dc65c6162746ca38cc4ab
 
 #nect step is to regex match each number + reactant name combo to be used when creating a list to search for the next set of items
 if __name__ == "__main__":
