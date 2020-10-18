8de3c58ce505afb200fdbdf197f0946a09e3c74d
Jack Winstanley-Punch <jackwp1996@gmail.com>
2020-10-18 23:15:22 +0100

    almost finished

--- day14.py
+++ day14.py
@@ -1,4 +1,5 @@
 import re
+from collections import defaultdict
 
 
 class Equation():
@@ -16,9 +17,9 @@ class Equation():
         for match in matches:
             value, name = match.split()
             reactants.append(name)
-            reactant_dict[name] = value
+            reactant_dict[name] = int(value)
         return reactants, reactant_dict
-    
+
     def __str__(self):
         return f"Product {self.product_value} {self.product} => {self.reactant_dict.keys(), self.reactant_dict.values()}"
 
@@ -27,6 +28,7 @@ class Storage():
     def __init__(self):
         self.equations = []
         self.simplify = []
+        self.temp_dict = defaultdict()
 
     def add_equation(self, equation):
         self.equations.append(equation)
@@ -34,8 +36,19 @@ class Storage():
     def add_simplify(self, simplify):
         self.equations.append(simplify)
 
+    def update_temp(self, id, value):
+        if id in self.temp_dict:
+            self.temp_dict[id] += value
+        else:
+            self.temp_dict[id] = value
 
-def fuel_parse(fuel_ls, storage):
+    def update_all(self, equation):
+        reactant_dict = equation.reactant_dict
+        for reactant in reactant_dict:
+            self.update_temp(reactant, reactant_dict[reactant])
+
+
+def fuel_parse(fuel_ls, storage): 
     lines = re.sub(",", "", fuel_ls)
     lines = lines.split("\n")
     for line in lines:
@@ -50,20 +63,28 @@ def simplify(equation):
 
 def foobar(storage):
     run = True
+    count = 0
     for equation in storage.equations:
         if equation.product == "FUEL":
             final_reaction = equation
     while run == True:
-
+    # find the equation round up and scale amount
         for equation in storage.equations:
             if equation.product in final_reaction.reactants:
+                final_reaction.update_all(equation)
                 final_reaction.reactants.remove(equation.product)
                 final_reaction.reactants.extend(equation.reactants)
                 # final_reaction.reactant_dict[equation.product] = equation.reactants
+                count += 1
         
-    return None #NOTHING
+        if len(final_reaction.reactants) == final_reaction.reactants.count("ORE"):
+            # print("Finished")
+            run = False
+    
+    return print(count)
 
-# ANSWER FOR THE EXAMPLE IS 165 ORE
+#I need a check to see if a dictionary entry already exists and then if it exists reevaluate the amount of the next product for the new amount and then update and those new ones are checked also
+# ANSWER FOR THE EXAMPLE IS 31 ORE
 
 
 #nect step is to regex match each number + reactant name combo to be used when creating a list to search for the next set of items
